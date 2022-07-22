cd1885e3663a1418e7f1ce3083c192d4b4c38927
histereza <terezateles41@gmail.com>
2022-07-22 10:38:06 -0300

    exercicio 3 modulo 2 curso de logica e oo (alteração no import)

--- bomba-relogio.py
+++ bomba-relogio.py
@@ -1,9 +1,9 @@
-import time
+from time import sleep
 
 seg = int(input("Indique um segundo inicial para a contagem: "))
 
 print("Iniciando contagem regressiva... ")
 for i in range(seg, 0, -1):
     print(i)
-    time.sleep(1)
+    sleep(1)
 print("BUM!!!")
