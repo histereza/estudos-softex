b44b8feb83859ef610e9976220bb648c056cc558
Tereza Teles <86265429+histereza@users.noreply.github.com>
2022-08-19 21:19:05 -0300

    exercicio 3 modulo 2 curso javascript

---
+++ exercicio3-mod2-js/calculo-terceira-nota.js
@@ -0,0 +1,6 @@
+var nota1 = Number(prompt('Informe sua primeira nota: '));
+var nota2 = Number(prompt('Informe sua segunda nota: '));
+var somaNotas = nota1 + nota2;
+var nota3 = 21 - somaNotas;
+
+console.log(`Para ser aprovado, sua próxima nota deverá ser, no mínimo, ${nota3}`);
